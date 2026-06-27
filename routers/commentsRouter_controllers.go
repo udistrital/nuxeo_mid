357681971ab23c5a77d13a0d2fff5d96405370d7
Lud <ludwingsanchezleon@gmail.com>
2026-06-27 00:06:47 -0500

    feat: despliegue ecr

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -7,31 +7,31 @@ import (
 
 func init() {
 
-    beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:ValidacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:ValidacionController"],
-        beego.ControllerComments{
-            Method: "Post",
-            Router: `/`,
-            AllowHTTPMethods: []string{"post"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:ValidacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:ValidacionController"],
+		beego.ControllerComments{
+			Method:           "Post",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
-    beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"] = append(beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"],
-        beego.ControllerComments{
-            Method: "Post",
-            Router: `/`,
-            AllowHTTPMethods: []string{"post"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"] = append(beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"],
+		beego.ControllerComments{
+			Method:           "Post",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
-    beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"] = append(beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"],
-        beego.ControllerComments{
-            Method: "Delete",
-            Router: `/:id`,
-            AllowHTTPMethods: []string{"delete"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"] = append(beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"],
+		beego.ControllerComments{
+			Method:           "Delete",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 }
